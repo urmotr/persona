09c75cb8beb90146c99caf29146b38be245a51cf
Urmot Rosenberg <urmotr@tlu.ee>
2020-10-24 19:02:18 +0300

    Menu Page
    Menu page with title, start button and exit button. Start changes the scene and exit would exit.

---
+++ Assets/Scripts/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persona : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Render_Colored_Rectangle(50, 50, 50, 50, 10, 10, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Render_Colored_Rectangle(int x, int y, int w, int h, float r, float g, float b)
+    {
+        Texture2D rgb_texture = new Texture2D(w, h);
+        Color rgb_color = new Color(r, g, b);
+        int i, j;
+        for (i = 0; i < w; i++)
+        {
+            for (j = 0; j < h; j++)
+            {
+                rgb_texture.SetPixel(i, j, rgb_color);
+            }
+        }
+        rgb_texture.Apply();
+        GUIStyle generic_style = new GUIStyle();
+        GUI.skin.box = generic_style;
+        GUI.Box(new Rect(x, y, w, h), rgb_texture);
+    }
+}
