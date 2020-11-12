c85c0e9531b3c0765a73ff838bee6b9465ef7646
Urmot Rosenberg <urmotr@tlu.ee>
2020-11-12 14:31:28 +0200

    Scene changes and code for changing scenes

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject playerA;
+    public GameObject playerB;
+
+    static string selectedPlayer = "";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Selected: " + selectedPlayer);
+        if(selectedPlayer != "") SelectCharacter(selectedPlayer);
+        DontDestroyOnLoad(transform.gameObject);
     }
 
     // Update is called once per frame
@@ -16,9 +25,19 @@ public class Menu : MonoBehaviour
     {
     }
 
-    public void StartGame()
+    public void SelectCharacter(string character)
     {
-        SceneManager.LoadScene("persona");
+        selectedPlayer = character;
+        if (character == "PlayerA")
+        {
+            playerA.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            playerB.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
+        } else
+        {
+            playerA.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
+            playerB.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
+        Debug.Log(character);
     }
 
 }
---
+++ Assets/Scripts/PersonaGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersonaGame : MonoBehaviour
+{
+
+    public void ChangeScene(string target)
+    {
+        SceneManager.LoadScene(target);
+    }
+}
